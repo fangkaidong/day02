6200508695050d04c074c9751c829520fbb4cea1
fangkaidong <202845721@qq.com>
2021-08-21 10:39:16 +0800

    分支提交第一次提交

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -3,5 +3,8 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("....");
+        System.out.println("哈哈");
+        System.out.println("perfect");
+        System.out.println("let's go");
     }
 }
